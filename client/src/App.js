83d03f1e13441c87e4211dbd1b501ae5ccde4703
Thanh Phuoc Nguyen <thanhphuocb7@gmail.com>
2023-07-10 19:07:30 +0700

    update logics

--- client/src/App.js
+++ client/src/App.js
@@ -11,15 +11,15 @@ function App() {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					{/* <p></p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
+					<p></p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
 					<Link to="/">Home</Link>
 					<Link to="/otherpage">Other Page</Link>
 				</header>
